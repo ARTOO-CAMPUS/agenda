93348502e1d1c1f6097e5bb3df1eab5b5cff7180
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-15 15:45:15 +0100

    prima rotta

---
+++ app.routes.js
@@ -0,0 +1,13 @@
+angular.module('app')
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+		$urlRouterProvider.when('', '/');
+
+		$stateProvider
+			.state('/', {
+				url: '/',
+				controller: 'AgendaCtrl',
+				templateUrl: 'agenda/agenda.template.html'
+			});
+
+	});
